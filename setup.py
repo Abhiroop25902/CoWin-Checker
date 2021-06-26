90e62bdf281f30bed422d9e8ddc435f3a0c0653f
Abhiroop25902 <69428539+Abhiroop25902@users.noreply.github.com>
2021-06-26 10:14:26 +0530

    fix: changed set to list,was causing error print

--- setup.py
+++ setup.py
@@ -14,22 +14,22 @@ cowin_fn.print_all_district(state_id)
 district_id = '395'
 district_name = 'Mumbai'
 
-# now run the below commented command with your district_id to get 
+# now run the below commented command with your district_id to get
 # all the centers pincode, center_id, and name
 
 cowin_fn.print_all_centers(district_id)
 
 # now select make your preferred list of centers to check
-specific_center_ids = {695695, 597000, 694629}
-specific_pin_codes = {400013, 400016, 400028}
-specific_names = {"The World Tower MCGM Parkg", "P D Hinduja 1",
-                  "KOHINOOR PUB PARKING (DRIVE)"}
+specific_center_ids = [695695, 694629, 614417, 597000]
+specific_pin_codes = [400013, 400028, 400016, 400016]
+specific_names = ["The World Tower MCGM Parkg",
+                  "KOHINOOR PUB PARKING (DRIVE)", "Mahim Municipal Maternity Home", "P D Hinduja 1"]
 
 
 print(f"state_id: {state_id}\nstate: {state_name}")
 print(f"district_id: {district_id}\ndistrict: {district_name}")
 
-for center_id,pincode,name in zip(specific_center_ids,specific_pin_codes,specific_names):
+for center_id, pincode, name in zip(specific_center_ids, specific_pin_codes, specific_names):
     print(f"center_id: {center_id}; pincode: {pincode}; center_name: {name}")
 
 print("If any of these information is wrong, please update your information in 'setup.py'.\n")
